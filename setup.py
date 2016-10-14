9004789a0f4ef9c641fe6d16164523da12ea1541
Justin Graves <jgraveskc@gmail.com>
2016-10-13 21:37:49 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Atlas API',
+	  version='1.0',
+	  description='Python library for interacting with the Infegy Atlas API',
+	  author='Infegy, Inc.',
+	  author_email='support@infegy.com',
+	  url='https://atlas.infegy.com',
+	  py_modules=['atlas_api'])
